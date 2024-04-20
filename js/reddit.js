20944a1134c52080f63894b24b542285fc0a7ebe
yywzl <93162394+yywzl@users.noreply.github.com>
2024-04-20 14:24:15 +0800

    更新 reddit.js

--- js/reddit.js
+++ js/reddit.js
@@ -1,14 +1,17 @@
 let body;
 try {
-  body = JSON.parse($response.body.replace(/"isNsfw":true/g, '"isNsfw":false'))
-  if (body.data?.children?.commentsPageAds) {
-    body.data.children.commentsPageAds = []
-  } 
-  for (const [k, v] of Object.entries(body.data)) {
-    if (v?.elements?.edges) {
-      body.data[k].elements.edges = v.elements.edges.filter(
-        i =>
-          !['AdPost'].includes(i?.node?.__typename) 
+body = JSON.parse($response.body.replace(/"isNsfw":true/g, '"isNsfw":false'))
+if 
+(body.data?.children?.commentsPageAds) {
+body.data.children.commentsPageAds = []
+} 
+for (const [k, v] of
+Object.entries(body.data)) {
+if (v?.elements?.edges) {
+body.data[k].elements.edges = v.elements.edges.filter(
+i =>
+!
+['AdPost'].includes(i?.node?.__typename) 
 &&
 !i?.node?.cells?.some(j => j?.__typename 
 === 'AdMetadataCell') &&
